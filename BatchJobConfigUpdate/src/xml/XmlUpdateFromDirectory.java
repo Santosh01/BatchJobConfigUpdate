e404954fd28f643cbd97bb56453b722412212515
SantoshKumar <SantoshKumar@Santosh.hsd1.ca.comcast.net>
2017-10-15 22:42:46 -0700

    Added function to generate log file

--- BatchJobConfigUpdate/src/xml/XmlUpdateFromDirectory.java
+++ BatchJobConfigUpdate/src/xml/XmlUpdateFromDirectory.java
@@ -3,12 +3,17 @@ package xml;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -17,7 +22,7 @@ import org.apache.commons.lang3.StringUtils;
 
 /**
  *<p>This class have been implemented to update job configuration files
- *(*.test.xml and *.test.template.xml) from 01, 02 batch jobs.
+ *(*.script.xml and *.template.xml) from FM01, FM02 batch jobs.
  *</p>
  * @author Santosh Kumar Dubey
  */
@@ -26,9 +31,74 @@ public class XmlUpdateFromDirectory
     //~ Methods ----------------------------------------------------------------
 
     /**
+    *
+    * <p>
+    *    This method prints the original data from the job configuration file (*.script.xml and *.template.xml).
+    *</p>
+    *
+    * @param targetFilePath
+    * @param jobName
+    *
+    * @throws IOException
+    */
+    private static void verifyScriptAndTemplateXMLOriginalData(
+        String targetFilePath, String jobName )
+      throws IOException
+    {
+        File targetFile = new File( targetFilePath );
+        String line;
+
+        for ( File f : targetFile.listFiles( ) )
+        {
+            if ( f.isFile( ) )
+            {
+                if ( f.getName( ).equals( jobName + templateFileExtension ) )
+                {
+                    BATCH_JOB_CONTENTS =
+                        new BufferedReader( new FileReader(
+                                targetFilePath + jobName +
+                                  templateFileExtension ) );
+
+                    while ( ( line = BATCH_JOB_CONTENTS.readLine( ) ) != null )
+                    {
+                        if ( line.contains( originalString ) &&
+                              StringUtils.startsWith( line.trim( ), tag ) )
+                        {
+                            System.out.format( format, timeStamp, jobName,
+                                jobName + templateFileExtension, line );
+                        }
+                    }
+                }
+                else
+                {
+                    if ( f.getName( ).equals( jobName + scriptFileExtension ) )
+                    {
+                        BATCH_JOB_CONTENTS =
+                            new BufferedReader( new FileReader(
+                                    targetFilePath + jobName +
+                                      scriptFileExtension ) );
+
+                        while ( ( line = BATCH_JOB_CONTENTS.readLine( ) ) !=
+                              null )
+                        {
+                            if ( line.contains( originalString ) &&
+                                  StringUtils.startsWith( line.trim( ), tag ) )
+                            {
+                                System.out.format( format, timeStamp, jobName,
+                                    jobName + scriptFileExtension, line );
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    /**
      *
      * <p>
-     *    This method verifies changes from the job configuration file (*.test.xml and *.test.template.xml).
+     *    This method verifies changes from the job configuration file (*.script.xml and *.template.xml).
      *</p>
      *
      * @param targetFilePath
@@ -59,19 +129,19 @@ public class XmlUpdateFromDirectory
                         if ( line.contains( updateToString ) &&
                               StringUtils.startsWith( line.trim( ), tag ) )
                         {
-                            System.out.println( jobName + ":" + line + " in " +
-                                  jobName + templateFileExtension );
+                            System.out.format( format, timeStamp, jobName,
+                                jobName + templateFileExtension, line );
                         }
                     }
                 }
                 else
                 {
-                    if ( f.getName( ).equals( jobName + testFileExtension ) )
+                    if ( f.getName( ).equals( jobName + scriptFileExtension ) )
                     {
                         BATCH_JOB_CONTENTS =
                             new BufferedReader( new FileReader(
                                     targetFilePath + jobName +
-                                      testFileExtension ) );
+                                      scriptFileExtension ) );
 
                         while ( ( line = BATCH_JOB_CONTENTS.readLine( ) ) !=
                               null )
@@ -79,8 +149,8 @@ public class XmlUpdateFromDirectory
                             if ( line.contains( updateToString ) &&
                                   StringUtils.startsWith( line.trim( ), tag ) )
                             {
-                                System.out.println( jobName + ":" + line +
-                                      " in " + jobName + testFileExtension );
+                                System.out.format( format, timeStamp, jobName,
+                                    jobName + scriptFileExtension, line );
                             }
                         }
                     }
@@ -92,7 +162,38 @@ public class XmlUpdateFromDirectory
 
     /**
      * <p>
-     *    This is runner method which forms *.test.xml and *.test.template.xml directory path
+     *    This is runner method which forms *.script.xml and *.template.xml directory path
+     *    and verifies the original data from the job configuration.
+     * </p>
+     *
+     * @param jobList
+     *
+     * @throws IOException
+     */
+    public static void verifyOriginalDataFromJobConfigurationFile(
+        List<String> jobList )
+      throws IOException
+    {
+        for ( String job : jobList )
+        {
+            for ( File f : dirList.listFiles( ) )
+            {
+                if ( f.isDirectory( ) )
+                {
+                    if ( f.getName( ).equals( job ) )
+                    {
+                        verifyScriptAndTemplateXMLOriginalData(
+                            absoluteJobDirPath + "\\" + job + "\\", job );
+                    }
+                }
+            }
+        }
+    }
+
+
+    /**
+     * <p>
+     *    This is runner method which forms *.script.xml and *.template.xml directory path
      *    and verifies the changes from the job configuration.
      * </p>
      *
@@ -100,7 +201,8 @@ public class XmlUpdateFromDirectory
      *
      * @throws IOException
      */
-    public static void verifyChangesFromJobConfigurationFile( List<String> jobList )
+    public static void verifyChangesFromJobConfigurationFile(
+        List<String> jobList )
       throws IOException
     {
         for ( String job : jobList )
@@ -122,7 +224,7 @@ public class XmlUpdateFromDirectory
 
     /**
      * <p>
-     *  This method will update the *.test.xml job configuration files.
+     *  This method will update the *.script.xml job configuration files.
      * </p>
      *
      * @param targetFilePath
@@ -144,7 +246,7 @@ public class XmlUpdateFromDirectory
         {
             BATCH_JOB_CONTENTS =
                 new BufferedReader( new FileReader(
-                        targetFilePath + jobName + testFileExtension ) );
+                        targetFilePath + jobName + scriptFileExtension ) );
 
             List<String> lines = new ArrayList<String>( );
             int index = 0;
@@ -170,7 +272,7 @@ public class XmlUpdateFromDirectory
             {
                 File oldFile =
                     new File( targetFilePath + "\\" + jobName +
-                          testFileExtension );
+                          scriptFileExtension );
 
                 oldFile.setWritable( true, true );
 
@@ -185,7 +287,7 @@ public class XmlUpdateFromDirectory
 
     /**
      * <p>
-     *  This method will update the *.test.template.xml job configuration files.
+     *  This method will update the *.template.xml job configuration files.
      * </p>
      *
      * @param targetFilePath
@@ -249,12 +351,12 @@ public class XmlUpdateFromDirectory
 
     /**
      * <p>
-     *    This is runner method to update *.test.xml and *.test.template.xml files.
+     *    This is runner method to update *.script.xml and *.template.xml files.
      *    <ul><u><b>Conditions: </b></u>
      *      <li>If the job directory is not present in the destination path then show it to user</li>
      *      <li>Check if the job has already updated with current changes</li>
      *      <li>If the few jobs are updated already and few not then program will handle this condition accordingly</li>
-     *      <li>If the destination job directory has *.test.xml then update this otherwise update both</li>
+     *      <li>If the destination job directory has *.script.xml then update this otherwise update both</li>
      *    </ul>
      * </p>
      *
@@ -340,21 +442,6 @@ public class XmlUpdateFromDirectory
 
                     break;
                 }
-                else
-                {
-                    if ( !job.equals( f.getName( ) ) &&
-                          !jobProcessStatusMap.containsKey( f.getName( ) ) )
-                    {
-                        if ( !jobProcessStatusMap.containsKey( f.getName( ) ) )
-                        {
-                            jobProcessStatusMap.put( job,
-                                new ArrayList<String>( ) );
-                        }
-
-                        jobProcessStatusMap.get( job ).add(
-                            "job directory not found" );
-                    }
-                }
 
                 isUpdated = false;
             }
@@ -366,7 +453,7 @@ public class XmlUpdateFromDirectory
 
     /**
      * <p>
-     *  This method will overwrite/create the *.test.xml and *.test.template.xml files with the new changes.
+     *  This method will overwrite/create the *.script.xml and *.template.xml files with the new changes.
      * </p>
      *
      * @param file
@@ -401,10 +488,27 @@ public class XmlUpdateFromDirectory
 
 
     /**
-     * <p>This is main method where all jobs (that need to be updated) will be added to a list.
+     * <p>This is the method where all jobs (that need to be updated) will be added to a list.
      *    <b>Only the stream directory should be added to the list not configuration file name.</b>
      *
      *  </p>
+     *
+    * @return List
+    */
+    private static List<String> feedInput( )
+    {
+        List<String> list = new ArrayList<String>( );
+
+
+        list.add( "ABC-000-000" );
+
+
+        return list;
+    }
+
+
+    /**
+     * <p> This is main method which process the changes.</p>
      *
      * @param args
      * @throws IOException
@@ -413,12 +517,26 @@ public class XmlUpdateFromDirectory
     public static void main( String[] args )
       throws IOException
     {
-        List<String> list = new ArrayList<String>( );
+        List<String> streamList = feedInput( );
 
-        list.add( "ABC-01" );
-        list.add( "XYZ-01" );
+        FileOutputStream f =
+            new FileOutputStream(
+                "..//BatchXmlConfigurationUpdate//src//log//log.txt" );
+        System.setOut( new PrintStream( f ) );
 
-        Map<String, ArrayList<String>> jobProcessStatusMap = doChangesInJobConfigurationFile( list );
+        System.out.println( "Pre-Process (Original Data) Verification:" );
+        System.out.println(
+            "-----------------------------------------------------------------------------------------------------------------------------------------------" );
+        verifyOriginalDataFromJobConfigurationFile( streamList );
+        System.out.println( );
+
+        
+        System.out.println( "Processing Changes:" );
+        System.out.println(
+            "-----------------------------------------------------------------------------------------------------------------------------------------------" );
+
+        Map<String, ArrayList<String>> jobProcessStatusMap =
+            doChangesInJobConfigurationFile( streamList );
 
         for ( Entry<String, ArrayList<String>> status :
               jobProcessStatusMap.entrySet( ) )
@@ -427,35 +545,55 @@ public class XmlUpdateFromDirectory
 
             for ( String job : jobList )
             {
-                System.out.println( status.getKey( ) + ": " + job );
+                System.out.format( formatForMain, timeStamp, status.getKey( ),
+                    job );
             }
         }
-        verifyChangesFromJobConfigurationFile( list );
+
+        
+        System.out.println( "\nPost-Process Verification:" );
+        System.out.println(
+            "-----------------------------------------------------------------------------------------------------------------------------------------------" );
+
+        verifyChangesFromJobConfigurationFile( streamList );
+        f.close( );
+        ps.println(
+            "Log file Generated(/BatchXmlConfigurationUpdate/src/log/log.txt)..." );
+        ps.println( "Please refresh log directory..." );
     }
 
     //~ Static variables -------------------------------------------------------
 
     /**
-     * Value of this static variable will the original string from the job configuration file.
+     * Value of this static variable will be the original string from the job configuration file.
      */
-    private static String originalString = "";
+    private static String originalString = "ABC";
 
     /**
      * Value of this static variable will be the new string value which we are intended
      * to update in the job configuration file.
      */
-    private static String updateToString = "";
+    private static String updateToString = "ABC.xml";
 
-    private static String templateFileExtension = "-test.template.xml";
-    private static String testFileExtension = "-test.xml";
-    private static String tag = "<import file=";
-    private static BufferedReader BATCH_JOB_CONTENTS = null;
+    /**
+     * Value of this static variable will be the tag name where you want to make changes.
+     */
+    private static String tag = "tag1";
 
     /**
      * Value of this static variable will be the location of the job configuration.
-     *  such as "C:\\workspace\\[stream]"
+     *  
      */
-    private static String absoluteJobDirPath =
-        "C:\\workspaces\\sample_for_delete";
+    
+    private static String absoluteJobDirPath = "C:\\rad_workspaces\\sample_for_delete";
     private static File dirList = new File( absoluteJobDirPath );
+    private static String timeStamp =
+        new SimpleDateFormat( "yyyy/MM/dd HH:mm:ss", Locale.US ).format(
+            new Date( ) );
+    private static BufferedReader BATCH_JOB_CONTENTS = null;
+    private static String templateFileExtension = "-template.xml";
+    private static String scriptFileExtension = "-script.xml";
+    private static PrintStream ps = System.out;
+    private static String format = "%1$-10s | %2$-5s | %3$-35s\t\t | %4$-30s\n";
+    private static String formatForMain = "%1$-10s | %2$-5s | %3$-35s\t\t\n";
 }
